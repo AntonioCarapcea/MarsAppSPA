4703e7828d3b4c96aa8354288bfb0a72f0595d26
Andrei Carapcea <Andrei.Carapcea@softwire.com>
2021-07-01 16:10:17 +0300

    Added routing and a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import './App.scss';
 import NasaInfo from './NasaInfo';
 import { Component1 } from './Component1';
+import { BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { Header } from "./Header";
 
 function App() {
   return (<div className="App">
-    <header className="App-header">
-      <NasaInfo />
-      <Component1 />
-      <p></p>
-    </header>
+    <div className="App-header">
+      <Router>
+          <Switch>
+            <Route path="/clicker">
+              <Header />
+              <p> </p>
+              <Component1 />
+            </Route>
+            <Route path="/">
+              <Header />
+              <NasaInfo />
+            </Route>
+          </Switch>
+      </Router>
+      
+    </div>
   </div>);
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.scss';
+import { Link } from "react-router-dom";
+export function Header() {
+    return (        
+        <div className="Header">
+            <table className="HeaderTable">
+                <tr>
+                    <th>
+                        <Link className="NavLink" to="/clicker">
+                            Clicker
+                        </Link>
+                    </th>
+                    <th>
+                    <Link className="NavLink" to="/">
+                            NASA
+                        </Link>
+                    </th>
+                </tr>
+            </table>
+
+        </div>
+    );
+}
