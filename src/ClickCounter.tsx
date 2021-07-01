c0d081d436fbc9682628bc5f6aaeb0cbfb1556ff
Andrei Carapcea <Andrei.Carapcea@softwire.com>
2021-07-01 13:15:28 +0300

    Added local storage for click count

--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 export function ClickCounter() {
     const [count, setCount] = useState(0);
 
+
+    function increment() {
+        const newCount = count + 1;
+        setCount(newCount);
+        localStorage.setItem("count", newCount.toString());
+    }
+
+    useEffect(() => {
+        const initialCount = localStorage.getItem("count");
+        if (initialCount) {
+            setCount(parseInt(initialCount));
+        }
+    }, []);
+
     return (
         <div>
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
+            <button onClick={increment}>
                 Click me
             </button>
         </div>
